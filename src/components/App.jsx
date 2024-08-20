9d54497ead327588b497908eefc5b0283d3e297b
mahmoud-alhol <mahmoud.asem.w@gmail.com>
2024-08-20 05:45:05 +0300

    v0.1

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Nav from "./Nav";
+import Main from "./Main";
+
+
+
+export default function App() {
+  
+
+ 
+
+  return (
+    <>
+      <Nav />
+      <Main />
+    </>
+  );
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function Nav() {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search />
+      <NumResults />
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+
+function Search() {
+  const [query, setQuery] = useState("");
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+
+function NumResults() {
+  return (
+    <p className="num-results">
+      Found <strong>X</strong> results
+    </p>
+  );
+}
+
+export default Nav;
