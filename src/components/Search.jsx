e7cbb911151fa3d3c1fbd07a756354a4fd33e8a1
mahmoud-alhol <mahmoud.asem.w@gmail.com>
2024-08-24 09:14:27 +0300

    added focus on search on Enter press down using useRef and useEffect with cleanup

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,6 +7,21 @@ function Search({ query, setQuery }) {
     inputEl.current.focus();
   }, []);
 
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.removeEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
